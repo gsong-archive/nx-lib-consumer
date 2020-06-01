97f2abcb722ad54c9496e5b9002c6c38695e64ff
George Song <george@monozuku.com>
2020-06-01 06:10:33 -0700

    Style tweaks

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
-import React from "react";
+/** @jsx jsx */
+import { jsx } from "@emotion/core";
 
 import { Ui } from "@gsong/ui";
 
-const App = () => <Ui />;
+const App = () => (
+  <main css={{ margin: "2rem" }}>
+    <h1>App</h1>
+    <section css={{ border: "1px solid lightgray", padding: "1rem" }}>
+      <Ui />
+    </section>
+  </main>
+);
 
 export default App;
